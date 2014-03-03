2e36b5385193aa6eabe6814f1a7643b42c69375d
Stephen Aghaulor <saghaulor@gmail.com>
2014-03-02 21:11:08 -0800

    Added Devise modules to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+   
+  devise :confirmable, :database_authenticatable, :lockable, 
+         :omniauthable, :registerable, :recoverable, :rememberable,
+         :timeoutable, :trackable, :validatable
+
   has_and_belongs_to_many :doors
   validates :email, presence: true, uniqueness: { case_sensitive: false}
   validates :first_name, presence: true
