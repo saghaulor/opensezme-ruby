ea38ff574f30f3eaa9461c52306157a26a419944
Stephen Aghaulor <saghaulor@gmail.com>
2014-03-02 19:17:17 -0800

    Added factory girl to test harness.

---
+++ spec/support/factory_girl.rb
@@ -0,0 +1,13 @@
+require 'database_cleaner'
+RSpec.configure do |config|
+  # additional factory_girl configuration
+
+  config.before(:suite) do
+    begin
+      DatabaseCleaner.start
+      FactoryGirl.lint
+    ensure
+      DatabaseCleaner.clean
+    end
+  end
+end
